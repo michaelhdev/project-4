473c5a70626d66ca8eb166fdbe254f6bfe1d0c0e
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-17 18:01:07 +0000

    Added test for index.html

---
+++ home/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestViews(TestCase):
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
