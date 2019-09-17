b2e9fdb6cf4e21da62845e8d0003a9f5fea9a03b
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-17 17:32:59 +0000

    Removed default secret key

--- uniattractor/settings.py
+++ uniattractor/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gzrqob(s6p6tg7qj6hi!8ns%#f3ix6c&!e$wuw^td5kr4=4ae+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
