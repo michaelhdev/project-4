45dad7958267313c9a428050a2b0f53a91d8108c
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-17 16:47:05 +0000

    Added development environment variable to settings

--- uniattractor/settings.py
+++ uniattractor/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get('DEVELOPMENT'):
+	development = True
+else:
+	development = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,9 +31,9 @@ SECRET_KEY = 'gzrqob(s6p6tg7qj6hi!8ns%#f3ix6c&!e$wuw^td5kr4=4ae+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), os.environ.get('HOSTNAME'), 'dcef83cc31af4beb87a9209e6babfc0e.vfs.cloud9.us-east-1.amazonaws.com']
 
-
+DEBUG = development
 # Application definition
 
 INSTALLED_APPS = [
@@ -73,15 +78,15 @@ WSGI_APPLICATION = 'uniattractor.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
