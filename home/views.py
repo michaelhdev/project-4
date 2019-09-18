63f27c7b99773b89a63f550919de7eaf607dd1ad
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-18 18:21:11 +0000

    Updated index.html to include login and logout

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def get_homepage(request):
-    
+    """Return the index.html file"""
     return render(request, "index.html")
