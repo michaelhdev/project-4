860ad60d9482fcaf8d94adf1359dafce2513e7ed
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-18 18:53:14 +0000

    Added logout messages

--- uniattractor/urls.py
+++ uniattractor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from home.views import get_homepage
-from users.view import logout
+from users.views import logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 # Create your views here.
 
 def logout(request):
     """Log the user out"""
     auth.logout(request)
+    messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
