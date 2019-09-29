2ef6343bb9d9ed916a5c3dde8e060fbde1730c96
EC2 Default User <ec2-user@ip-172-31-24-232.ec2.internal>
2019-09-29 12:32:24 +0000

    Added required login to all views in the cart app

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def view_cart(request):
     """A View that renders the cart contents page"""
     return render(request, "cart.html")
 
-
+@login_required
 def add_to_cart(request, id):
     """Add a donation for a feature to the cart"""
     
@@ -20,7 +22,7 @@ def add_to_cart(request, id):
     messages.success(request, f'Donation added, ready for checkout')
     return redirect(reverse('get_features'))
 
-
+@login_required
 def delete_from_cart(request, id):
     """
     Deletes the donation from the cart
